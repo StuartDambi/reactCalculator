d4f0866e44ef05d459fe49216a988974a87511c3
StuartDambi <stuartdambi@gmail.com>
2020-03-12 12:26:54 +0300

    initial release

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Keypad from './components/Keypad';
+import Result from './components/Result';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: ''
+    }
+    this.calculate = this.calculate.bind(this);
+    this.reset = this.reset.bind(this);
+    this.backspace = this.backspace.bind(this);
+  }
+
+  calculate = () => {
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: "Error"
+      })
+    }
+  };
+
+  // Reset the calculator
+  reset = () => {
+    this.setState({
+      result: ""
+    });
+  }
+
+  // Delete function
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    });
+  }
+  // Convert to Dollars
+  // changeToDollars = () => {
+
+  //   this.state.result
+  // }
+
+
+  // OnClick functions
+  onClick = button => {
+    if (button === '=') {
+      this.calculate();
+    }
+    else if (button === 'CE') {
+      this.reset();
+    }
+    else if (button === 'DEL') {
+      this.backspace();
+    }
+    else if (button === 'Dollars') {
+
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      });
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Calculator</p>
+        </header>
+  
+        <Result result={this.state.result} />
+  
+        <Keypad onClick={this.onClick}/>
+      </div>
+    );
+  }
 }
 
 export default App;
